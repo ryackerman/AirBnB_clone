64b88ba63c0282409af04f37e205b643133bd9b7
Khalid TALEB <t.khalidtaleb@gmail.com>
2023-08-14 15:42:32 -0700

    5. Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ This Module contains a definition for BaseModel Class
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -26,6 +27,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """should print/str representation of the BaseModel instance."""
@@ -35,6 +37,7 @@ class BaseModel:
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
